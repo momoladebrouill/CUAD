c5e6b2f586e41e7ffe7b57b49c49388b3599351b
momoladebrouill <momoladebrouill@gmail.com>
2024-04-11 21:48:47 +0200

    Ça fonctionne un peu et d'un coup POF 🦩️

--- main.cu
+++ main.cu
@@ -1,42 +1,92 @@
 #include "raylib.h"
 #include <stdio.h>
+#define HEIGHT 450
+#define WIDTH 800
+#define N 254
+#define DT 0.1
+using namespace std;
 
-__global__ void add(int *a, int *b, int *c) {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    c[i] = a[i] + b[i];
+typedef struct vec {
+      float x;
+      float y;
+} vec;
+
+typedef struct point {
+     vec pos;
+     vec oldpos;
+     vec acc;
+} point;
+
+__device__ void add(vec * a, vec * b) {
+    a->x += b->x;
+    a->y += b->y;
 }
 
-__managed__ int vector_a[256], vector_b[256], vector_c[256];
+__device__ void mult(vec * a, float b) {
+    a->x *= b;
+    a->y *= b;
+}
 
-int main(){
-  // Make CUDA work
-  for (int i = 0; i < 256; i++) {
-    vector_a[i] = i;
-    vector_b[i] = 256-i;
+__device__ void copy(vec * dst, vec * src) {
+    dst->x = src->x;
+    dst->y = src->y;
+}
+
+__global__ void update(point *points) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    // Compute acceleration
+    add(&points[i].pos,&points[i].acc);
+    // Update position
+    point p = points[i];
+    vec * old_p = (vec*) malloc(sizeof(vec));
+    copy(old_p,&p.pos);
+
+    p.acc.y = 1.0;
+    mult(&p.pos, 2.0);
+    mult(&p.oldpos,-1.0);
+    add(&p.pos,&p.oldpos);
+    mult(&p.acc,DT*DT);
+    add(&p.pos, &p.acc);
+    copy(&p.oldpos, old_p);
+
+    if(p.pos.y > HEIGHT){
+      p.pos.y = 0;
+      p.oldpos.y = 0;
+    }
+    points[i] = p;
+ 
+}
+
+__managed__ point d_points[N];
+
+
+__host__ int main(){
+  SetTraceLogLevel(5); 
+
+  for (int i = 0; i < N; i++) {
+    float y = abs(rand()) % HEIGHT;
+    d_points[i].pos.x = i * 3.0;
+    d_points[i].pos.y = y;
+    d_points[i].oldpos.x = i * 3.0;
+    d_points[i].oldpos.y = y;
+    d_points[i].acc.y = 0;
+    d_points[i].acc.x = 0;
   }
-  /* blocks and threads per block */
-  add<<<1, 256>>>(vector_a, vector_b, vector_c);
-  cudaDeviceSynchronize();
 
-  int result_sum = 0;
-  for (int i = 0; i < 256; i++) {
-    result_sum += vector_c[i];
-  }
 
-  printf("Sum of vector_a and vector_b is %d\n", result_sum);
-  printf("Expected result is 256*255/2 = 32640\n");
-  // Make raylib work
-  const int screenWidth = 800;
-  const int screenHeight = 450;
-
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+  InitWindow(WIDTH, HEIGHT, "i don't know");
   SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
   while (!WindowShouldClose())    // Detect window close button or ESC key
   {
       BeginDrawing();
-      ClearBackground(RAYWHITE);
-      DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+      ClearBackground(BLACK);
+      for(int i=0;i<N;i++)
+        DrawCircle(d_points[i].pos.x, d_points[i].pos.y, 5.0, BLUE);
       EndDrawing();
+      update<<<1, N>>>(d_points);
+      printf("debut");
+      cudaDeviceSynchronize();
+      printf(" fin");
   }
   CloseWindow();        
   return 0;
